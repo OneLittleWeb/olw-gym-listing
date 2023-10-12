d7413fd009796bf5254d921b394daa5bc6b79924
anower52 <anower.hasan@gmail.com>
2023-10-12 10:51:00 +0600

    Duplicate city check in database, also check for states from directory to DB.

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -14,14 +14,12 @@ class CityController extends Controller
         $cities = City::orderBy('name')->get();
         $city = null;
         $category = null;
-        return view('city.index', compact('cities', 'city','category'));
+        return view('city.index', compact('cities', 'city', 'category'));
     }
 
     public function importCityData()
     {
         try {
-            City::truncate();
-
             $baseDirectory = 'H:\gym';
             if (!File::exists($baseDirectory)) {
                 throw new \Exception('Base directory does not exist.');
@@ -43,21 +41,69 @@ class CityController extends Controller
 
                 foreach ($cityDirectories as $cityDirectory) {
                     $cityName = trim(basename($cityDirectory), " ");
-                    $city = new City();
-                    $city->name = Str::lower($cityName);
-                    $city->slug = Str::slug($cityName);
-                    $city->background_image = Str::slug($cityName) . '.png';
-                    $city->state_id = $stateId;
-                    $city->save();
+                    $cityNameLower = Str::lower($cityName);
+
+                    // Check if the city already exists in the database
+                    $existingCity = City::where('name', $cityNameLower)->where('state_id', $stateId)->first();
+
+                    if (!$existingCity) {
+                        $city = new City();
+                        $city->name = $cityNameLower;
+                        $city->slug = Str::slug($cityNameLower);
+                        $city->background_image = Str::slug($cityNameLower) . '.png';
+                        $city->state_id = $stateId;
+                        $city->save();
+                    }
                 }
             }
 
-            // Success message
             return redirect()->back()->with('success', 'Cities imported successfully.');
         } catch (\Exception $e) {
-            // Handle exceptions
             return redirect()->back()->with('error', 'Error: ' . $e->getMessage());
         }
     }
+
+
+//    public function importCityData()
+//    {
+//        try {
+//
+//            $baseDirectory = 'H:\gym';
+//            if (!File::exists($baseDirectory)) {
+//                throw new \Exception('Base directory does not exist.');
+//            }
+//
+//            $stateDirectories = File::directories($baseDirectory);
+//
+//            foreach ($stateDirectories as $stateDirectory) {
+//                $stateName = trim(basename($stateDirectory), " ");
+//                $state = State::where('name', Str::lower($stateName))->first();
+//
+//                if (!$state) {
+//                    throw new \Exception('State not found: ' . $stateName);
+//                }
+//
+//                $stateId = $state->id;
+//
+//                $cityDirectories = File::directories($stateDirectory);
+//
+//                foreach ($cityDirectories as $cityDirectory) {
+//                    $cityName = trim(basename($cityDirectory), " ");
+//                    $city = new City();
+//                    $city->name = Str::lower($cityName);
+//                    $city->slug = Str::slug($cityName);
+//                    $city->background_image = Str::slug($cityName) . '.png';
+//                    $city->state_id = $stateId;
+//                    $city->save();
+//                }
+//            }
+//
+//            // Success message
+//            return redirect()->back()->with('success', 'Cities imported successfully.');
+//        } catch (\Exception $e) {
+//            // Handle exceptions
+//            return redirect()->back()->with('error', 'Error: ' . $e->getMessage());
+//        }
+//    }
 
 }
--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -17,24 +17,52 @@ class StateController extends Controller
         $city = null;
         $category = null;
 
-        return view('state.index', compact('states','cities', 'city','category'));
+        return view('state.index', compact('states', 'cities', 'city', 'category'));
     }
 
     public function importStateName()
     {
-        $state_directories = File::directories('H:\gym');
+        try {
+            $stateDirectories = File::directories('H:\gym');
 
-        foreach ($state_directories as $state_directory) {
-            $state_name = trim(basename($state_directory), " ");
-            $state = new State();
-            $state->name = $state_name;
-            $state->slug = Str::slug($state_name);
-            $state->background_image = Str::slug($state_name) . '.png';
-            $state->save();
+            foreach ($stateDirectories as $stateDirectory) {
+                $stateName = trim(basename($stateDirectory), " ");
+                $stateNameLower = Str::lower($stateName);
+
+                // Check if the state already exists in the database
+                $existingState = State::where('name', $stateNameLower)->first();
+
+                if (!$existingState) {
+                    $state = new State();
+                    $state->name = $stateNameLower;
+                    $state->slug = Str::slug($stateNameLower);
+                    $state->background_image = Str::slug($stateNameLower) . '.png';
+                    $state->save();
+                }
+            }
+
+            return redirect()->back()->with('success', 'States imported successfully.');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Error: ' . $e->getMessage());
         }
-
-        alert()->success('success', 'States imported successfully.');
-
-        return redirect()->back();
     }
+
+
+//    public function importStateName()
+//    {
+//        $state_directories = File::directories('H:\gym');
+//
+//        foreach ($state_directories as $state_directory) {
+//            $state_name = trim(basename($state_directory), " ");
+//            $state = new State();
+//            $state->name = $state_name;
+//            $state->slug = Str::slug($state_name);
+//            $state->background_image = Str::slug($state_name) . '.png';
+//            $state->save();
+//        }
+//
+//        alert()->success('success', 'States imported successfully.');
+//
+//        return redirect()->back();
+//    }
 }
