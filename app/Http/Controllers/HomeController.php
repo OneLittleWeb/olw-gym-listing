7c48e66f9cfdaf6985e3c77150f2656b9c6ca010
anower52 <anower.hasan@gmail.com>
2023-11-20 15:16:44 +0600

    Get blog posts.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,9 +28,9 @@ class HomeController extends Controller
             ->orderByDesc('total_views')
             ->take(4)
             ->get();
-//        $post = Post::taxonomy('category', 'things-to-do')->newest()->published()->take(6)->get();
-//
-//        dd($post);
+        $posts = Post::taxonomy('category', 'things-to-do')->newest()->published()->take(6)->get();
+
+        dd($posts);
 
         try {
             $posts = Post::taxonomy('category', 'things-to-do')->newest()->published()->take(6)->get();
