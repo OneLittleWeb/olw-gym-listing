163ac5a0ef149620cac4ad37ef19b173817287aa
anower52 <anower.hasan@gmail.com>
2023-11-27 12:29:41 +0600

    Removr unnecessary comments.

--- resources/views/organization/show.blade.php
+++ resources/views/organization/show.blade.php
@@ -942,19 +942,19 @@
                                             @endif
                                         </ul>
                                     </div>
-                                </div><!-- end card-item -->
+                                </div>
                             @endforeach
-                        </div><!-- end card-carousel -->
-                    </div><!-- end col-lg-12 -->
-                </div><!-- end row -->
-            </div><!-- end container -->
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="svg-bg svg-bg-3 z-index-negative">
                 <svg class="h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                     <path fill="#F5F7FC" fill-opacity="1"
                           d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                 </svg>
             </div>
-        </section><!-- end card-area -->
+        </section>
     @endif
     <!-- ================================
         END CARD AREA
