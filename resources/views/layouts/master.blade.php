d1a2fda377f0d2c137d48bdb7c913b972da354ff
anower52 <anower.hasan@gmail.com>
2023-12-06 18:25:38 +0600

    Google tag manager added.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -62,14 +62,13 @@
         </script>
 
         <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0RTN18CDQC"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S6GMWZHSYY"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                dataLayer.push(arguments);
-            }
+            function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-0RTN18CDQC');
+
+            gtag('config', 'G-S6GMWZHSYY');
         </script>
     @endproduction
 </head>
