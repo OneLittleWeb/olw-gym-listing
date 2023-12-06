3e322dff2b1bdb66ad1fd15c1da9757ef4322268
anower52 <anower.hasan@gmail.com>
2023-12-06 18:32:24 +0600

    Clarity tracking added.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,16 +48,18 @@
     @yield('json-ld')
     @production
         <!-- Clarity tracking code for https://gymnearx.com/ -->
-        <script> (function (c, l, a, r, i, t, y) {
+        <script type="text/javascript">
+            (function (c, l, a, r, i, t, y) {
                 c[a] = c[a] || function () {
                     (c[a].q = c[a].q || []).push(arguments)
                 };
                 t = l.createElement(r);
                 t.async = 1;
-                t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+                t.src = "https://www.clarity.ms/tag/" + i;
                 y = l.getElementsByTagName(r)[0];
                 y.parentNode.insertBefore(t, y);
-            })(window, document, "clarity", "script", "k1oa839p2j");</script>
+            })(window, document, "clarity", "script", "k1ob40apkc");
+        </script>
 
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-S6GMWZHSYY"></script>
