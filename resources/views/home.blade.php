870b60058277cc4965bf83065eee166b1905ed72
anower52 <anower.hasan@gmail.com>
2023-12-27 15:51:02 +0600

    Home page title modified gym to fitness center.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
                     <div class="hero-heading text-center">
                         <div class="section-heading">
                             <h1 class="sec__title cd-headline slide">
-                                Find America’s Best Gym Near
+                                Find America’s Best Fitness Center Near
                                 <span class="cd-words-wrapper py-0 header-text-decoration">
                                     <b class="is-visible">California</b>
                                     <b>Florida</b>
