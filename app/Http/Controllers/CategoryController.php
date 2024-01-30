aff865bcfb90903977d38ae975c60230a99de361
anower52 <anower.hasan@gmail.com>
2024-01-30 12:30:56 +0600

    Without cache, handle state and city.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -41,13 +41,17 @@ class CategoryController extends Controller
                 ->orderBy('category_count', 'desc')
                 ->get();
 
-            $states = Cache::rememberForever('states_with_organizations', function () {
-                return State::with('organizations')->get();
-            });
+//            $states = Cache::rememberForever('states_with_organizations', function () {
+//                return State::with('organizations')->get();
+//            });
 
-            $cities = Cache::remember('cities_with_states_' . $s_state->id, now()->addMinutes(60), function () use ($s_state) {
-                return City::with('state')->where('state_id', $s_state->id)->get();
-            });
+            $states = State::with('organizations')->get();
+
+//            $cities = Cache::remember('cities_with_states_' . $s_state->id, now()->addMinutes(60), function () use ($s_state) {
+//                return City::with('state')->where('state_id', $s_state->id)->get();
+//            });
+
+            $cities = City::with('state')->where('state_id', $s_state->id)->get();
 
             $organization_category_count = Organization::where('state_id', $s_state->id)
                 ->where('permanently_closed', 0)
