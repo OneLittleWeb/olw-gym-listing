22400fd80b1796a12f4ab1523a9a6ec59373252c
anower52 <anower.hasan@gmail.com>
2024-01-30 12:32:50 +0600

    For city wise organization handle without cache for the states and city.

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -41,13 +41,17 @@ class CityController extends Controller
             $s_state = State::where('slug', $state_slug)->first();
             $city = City::where('state_id', $s_state->id)->where('slug', $city_slug)->first();
 
-            $states = Cache::rememberForever('states_with_organizations_city', function () {
-                return State::with('organizations')->get();
-            });
+//            $states = Cache::rememberForever('states_with_organizations_city', function () {
+//                return State::with('organizations')->get();
+//            });
 
-            $cities = Cache::remember('cities_with_states_city_' . $s_state->id, now()->addMinutes(60), function () use ($s_state) {
-                return City::with('state')->where('state_id', $s_state->id)->get();
-            });
+            $states = State::with('organizations')->get();
+
+//            $cities = Cache::remember('cities_with_states_city_' . $s_state->id, now()->addMinutes(60), function () use ($s_state) {
+//                return City::with('state')->where('state_id', $s_state->id)->get();
+//            });
+
+            $cities = City::with('state')->where('state_id', $s_state->id)->get();
 
             $organizations = Organization::with('city:id,name,slug', 'state:name', 'category')
                 ->where('organization_category_slug', $organization_category_slug)
