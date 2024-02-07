be87568b1870a57c8823a2cbd84ef492c7efabd1
anower52 <anower.hasan@gmail.com>
2024-02-07 12:23:33 +0600

    State with cached for the state wise organizations.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -42,11 +42,11 @@ class CategoryController extends Controller
                 ->get();
 
             // Cache the states with organizations
-            $states = Cache::remember('states_with_organizations', now()->addHours(24), function () {
-                return State::with('organizations')->get();
-            });
+//            $states = Cache::remember('states_with_organizations', now()->addHours(24), function () {
+//                return State::with('organizations')->get();
+//            });
 
-//            $states = State::with('organizations')->get();
+            $states = State::with('organizations')->get();
 
             $cities = City::with('state')->where('state_id', $s_state->id)->get();
 
