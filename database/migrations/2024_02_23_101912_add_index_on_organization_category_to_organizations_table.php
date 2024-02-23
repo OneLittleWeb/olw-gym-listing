c050c7145d9986123f3da498c729bb814e5e634a
anower52 <anower.hasan@gmail.com>
2024-02-23 16:35:53 +0600

    Organizations category and category slug added in migrations.

---
+++ database/migrations/2024_02_23_101912_add_index_on_organization_category_to_organizations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexOnOrganizationCategoryToOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->index(['organization_category'], 'organizations_organization_category_index');
+            $table->index(['organization_category_slug'], 'organizations_organization_category_slug_index');
+            $table->index(['rate_stars'], 'organizations_rate_stars_index');
+            $table->index(['reviews_total_count'], 'organizations_reviews_total_count_index');
+            $table->index(['organization_address'], 'organizations_organization_address_index');
+            $table->index(['permanently_closed'], 'organizations_permanently_closed_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropIndex('organizations_organization_category_index');
+            $table->dropIndex('organizations_organization_category_slug_index');
+            $table->dropIndex('organizations_rate_stars_index');
+            $table->dropIndex('organizations_reviews_total_count_index');
+            $table->dropIndex('organizations_organization_address_index');
+            $table->dropIndex('organizations_permanently_closed_index');
+        });
+    }
+}
