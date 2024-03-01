dfbe8a309ba42901d05a85f3c6db9ff4c763fb9f
anower52 <anower.hasan@gmail.com>
2024-03-01 15:06:14 +0600

    Meta description modified for thebusiness profile.

--- resources/views/organization/show.blade.php
+++ resources/views/organization/show.blade.php
@@ -5,7 +5,7 @@
         <meta name="robots" content="noindex, follow">
     @endsection
 @endif
-@section('meta_description', str_replace("'","",$organization->organization_name) . " is in " . $organization->State ?? $organization->State->name . ", $city->name. Get photos, business hours, phone numbers, ratings, reviews and service details.")
+@section('meta_description', str_replace("'","",$organization->organization_name) . " is in " . $organization->State->name . ", $city->name. Get photos, business hours, phone numbers, ratings, reviews and service details.")
 @section('meta_keywords',  str_replace("'","",$organization->organization_name). ", " .str_replace("'","",$organization->organization_name) . " review")
 @section('content')
     <!-- ======START FULL SCREEN SLIDER===== -->
