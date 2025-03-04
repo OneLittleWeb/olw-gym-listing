b54ac5d9fb80971d410a37caaac54d941313248c
One Little Web <122091685+OneLittleWeb@users.noreply.github.com>
2025-03-04 11:57:58 +0600

    Update 2024_02_23_101912_add_index_on_organization_category_to_organizations_table.php

--- database/migrations/2024_02_23_101912_add_index_on_organization_category_to_organizations_table.php
+++ database/migrations/2024_02_23_101912_add_index_on_organization_category_to_organizations_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,13 +15,14 @@ class AddIndexOnOrganizationCategoryToOrganizationsTable extends Migration
     public function up()
     {
         Schema::table('organizations', function (Blueprint $table) {
-            $table->index(['organization_category(191)'], 'organizations_organization_category_index');
-            $table->index(['organization_category_slug(191)'], 'organizations_organization_category_slug_index');
             $table->index(['rate_stars'], 'organizations_rate_stars_index');
             $table->index(['reviews_total_count'], 'organizations_reviews_total_count_index');
-            $table->index(['organization_address(191)'], 'organizations_organization_address_index');
             $table->index(['permanently_closed'], 'organizations_permanently_closed_index');
         });
+
+        DB::statement('ALTER TABLE organizations ADD INDEX organizations_organization_category_index (organization_category(191))');
+        DB::statement('ALTER TABLE organizations ADD INDEX organizations_organization_category_slug_index (organization_category_slug(191))');
+        DB::statement('ALTER TABLE organizations ADD INDEX organizations_organization_address_index (organization_address(191))');
     }
 
     /**
@@ -38,5 +40,10 @@ class AddIndexOnOrganizationCategoryToOrganizationsTable extends Migration
             $table->dropIndex('organizations_organization_address_index');
             $table->dropIndex('organizations_permanently_closed_index');
         });
+
+        // Drop indexes for TEXT columns
+        DB::statement('ALTER TABLE organizations DROP INDEX organizations_organization_category_index');
+        DB::statement('ALTER TABLE organizations DROP INDEX organizations_organization_category_slug_index');
+        DB::statement('ALTER TABLE organizations DROP INDEX organizations_organization_address_index');
     }
 }
