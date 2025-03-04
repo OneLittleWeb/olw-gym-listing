0a3cd2ba3a669f93961ab635fc4b14b3e35d053f
One Little Web <122091685+OneLittleWeb@users.noreply.github.com>
2025-03-04 11:53:16 +0600

    Update 2024_02_23_101912_add_index_on_organization_category_to_organizations_table.php

--- database/migrations/2024_02_23_101912_add_index_on_organization_category_to_organizations_table.php
+++ database/migrations/2024_02_23_101912_add_index_on_organization_category_to_organizations_table.php
@@ -14,11 +14,11 @@ class AddIndexOnOrganizationCategoryToOrganizationsTable extends Migration
     public function up()
     {
         Schema::table('organizations', function (Blueprint $table) {
-            $table->index(['organization_category'], 'organizations_organization_category_index');
-            $table->index(['organization_category_slug'], 'organizations_organization_category_slug_index');
+            $table->index(['organization_category(191)'], 'organizations_organization_category_index');
+            $table->index(['organization_category_slug(191)'], 'organizations_organization_category_slug_index');
             $table->index(['rate_stars'], 'organizations_rate_stars_index');
             $table->index(['reviews_total_count'], 'organizations_reviews_total_count_index');
-            $table->index(['organization_address'], 'organizations_organization_address_index');
+            $table->index(['organization_address(191)'], 'organizations_organization_address_index');
             $table->index(['permanently_closed'], 'organizations_permanently_closed_index');
         });
     }
