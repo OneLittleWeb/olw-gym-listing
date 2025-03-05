cee3ad2fe680316cddf1825fd0310dc88dbe58f4
Khabirul Basar <kbtonmoy641@gmail.com>
2025-03-05 15:11:00 +0600

    Clarity Disabled

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -49,18 +49,22 @@
     @yield('json-ld')
     @production
         <!-- Clarity tracking code for https://gymnearx.com/ -->
-        <script type="text/javascript">
-            (function (c, l, a, r, i, t, y) {
-                c[a] = c[a] || function () {
-                    (c[a].q = c[a].q || []).push(arguments)
-                };
-                t = l.createElement(r);
-                t.async = 1;
-                t.src = "https://www.clarity.ms/tag/" + i;
-                y = l.getElementsByTagName(r)[0];
-                y.parentNode.insertBefore(t, y);
-            })(window, document, "clarity", "script", "k1ob40apkc");
-        </script>
+       {{-- <script type="text/javascript">
+            window.addEventListener('load', function () {
+                setTimeout(function () {
+                    (function (c, l, a, r, i, t, y) {
+                        c[a] = c[a] || function () {
+                            (c[a].q = c[a].q || []).push(arguments)
+                        };
+                        t = l.createElement(r);
+                        t.async = 1;
+                        t.src = "https://www.clarity.ms/tag/" + i;
+                        y = l.getElementsByTagName(r)[0];
+                        y.parentNode.insertBefore(t, y);
+                    })(window, document, "clarity", "script", "k1ob40apkc");
+                }, 2000); // Delay Clarity by 2 seconds
+            });
+        </script>--}}
 
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-S6GMWZHSYY"></script>
